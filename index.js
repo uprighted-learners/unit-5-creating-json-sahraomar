0ae220ecfa9a1c63e7540c871af487012d0f3770
sahraomar <sahraaliwrites@gmail.com>
2024-07-11 09:13:27 -0400

    assignemnet complete

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require("path"); //path module required
 const app = express();
 
 const PORT = 3000;
 
+// route to server the JSON file
+app.get("/api/answers", (req, res) => {
+  res.sendFile(path.join(__dirname, "api", "answers.json")); // Use __dirname with two underscores
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
